efd4bdab5fdda9b86dabef5213593ed91d4c9467
tashilhamo55 <83581317+tashilhamo55@users.noreply.github.com>
2024-06-03 14:23:32 +0600

    g

--- lib/screens/account/addaccount.dart
+++ lib/screens/account/addaccount.dart
@@ -15,73 +15,55 @@ class _AddAccountState extends State<AddAccount> {
   Widget build(BuildContext context) {
     return Container(
         child: Column(
-          
-          children: [
-            Center(
-              child: ClipOval(
-                child: Image.network(
-                  'https://cdn.pixabay.com/photo/2021/07/24/07/23/chow-chow-6488846_1280.jpg',
-                  width: 150,  
-                  height: 150, 
-                  fit: BoxFit.cover,
-                ),
+      children: [
+        Center(
+          child: ClipOval(
+            child: Image.network(
+              'https://cdn.pixabay.com/photo/2021/07/24/07/23/chow-chow-6488846_1280.jpg',
+              width: 150,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Account Name'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Type'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Opening Balance'),
+          ),
+        ),
+        Align(
+          child: Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 150),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Confirm',
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
               ),
             ),
-
-
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-                  // controller: _username,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Account Name'),
-                ),
           ),
-
-
-            Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-                  // controller: _username,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Type'),
-                ),
-          ),
-
-
-
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-                  // controller: _username,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Opening Balance'),
-                ),
-          ),
-
-             Align(
-                child: Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(top: 150),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Confirm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      // Background color
-                    ),
-                  ),
-                ),
-                           )
-          
-        ],
-
-          
         )
-      
-    );
+      ],
+    ));
   }
 }
